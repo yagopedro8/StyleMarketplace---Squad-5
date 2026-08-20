98dc6b6ce03a0dbaf5d54e879476ce6c5ec91c68
yagopedro8 <yagobtn8@gmail.com>
2026-08-20 17:08:58 -0300

    Correção de conflitos realocando pastas e código

--- src/models/migrations/20260819022629_add_variant/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Variant" (
-    "id" SERIAL NOT NULL,
-    "color" TEXT NOT NULL,
-    "size" TEXT NOT NULL,
-    "stock" INTEGER NOT NULL,
-
-    CONSTRAINT "Variant_pkey" PRIMARY KEY ("id")
-);
