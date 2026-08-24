985312c51ac04b4b248310df231490ee01cf3400
Arthur Amorim <kenjustken@MacBook-Air-de-Arthur.local>
2026-08-24 18:16:02 -0300

    feat: adiciona upload de arquivos com multer

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { uploadSingle } from "../config/uploader";
 
 import {
   createUser,
@@ -71,9 +72,10 @@ router.delete(
 router.post(
   "/products",
   authMiddleware,
-  validate(createProductSchema),
+  uploadSingle,
   createProduct
 );
+
 
 router.get(
   "/products",
@@ -88,7 +90,7 @@ router.get(
 router.put(
   "/products/:id",
   authMiddleware,
-  validate(updateProductSchema),
+  uploadSingle,
   updateProduct
 );
 
