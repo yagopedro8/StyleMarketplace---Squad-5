ca6c38b72e476d8961d947c5fe3dbea77e923b88
Guilherme <guilherme.contatoprofissional21@gmail.com>
2026-08-28 00:39:24 -0300

    fix: rotas das telas

--- front/src/routes/Routes.tsx
+++ front/src/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { SignInPage } from "../pages/SignInPage";
 import { SignUpPage } from "../pages/SignUpPage";
 import { HomePage } from "../pages/HomePage";
@@ -10,13 +10,13 @@ export default function AppRoutes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/signin" replace />} />
-        <Route element={<SignInPage />} path="/signin" />
-        <Route element={<SignUpPage />} path="/signup" />
-        <Route element={<HomePage />} path="/home" />
-        <Route element={<SalePage />} path="/sale" />
-        <Route element={<CartPage />} path="/cart" />
-        <Route element={<ProductPage />} path="/product/:productId" />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/sale" element={<SalePage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/product/:productId" element={<ProductPage />} />
       </Routes>
     </Router>
   );
