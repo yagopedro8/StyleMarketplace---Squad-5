d5f8d4b2bb32a91cf76648bc030f9008eca6d8a2
Arthur Amorim <kenjustken@MacBook-Air-de-Arthur.local>
2026-08-27 22:33:20 -0300

    refactor: atualiza validacao de order

--- back/src/schemas/orderSchema.ts
+++ back/src/schemas/orderSchema.ts
@@ -1,11 +1,9 @@
 import { z } from "zod";
 
 export const createOrderSchema = z.object({
-  status: z.string().min(1, "O status é obrigatório."),
-  totalValue: z.number(),
   userId: z.number(),
 });
 
-export const updateOrderSchema = createOrderSchema
-  .omit({ userId: true })
-  .partial();
+export const updateOrderSchema = z.object({
+  status: z.string().min(1, "O status é obrigatório."),
+}).partial();
